604f3fb6422e05b9df29dea71d18e1b55b5c5153
Dulan Jayasuriya <dulanj@zone24x7.com>
2018-05-29 14:52:05 +0530

    Webcam code added

---
+++ code/test_webcam.py
@@ -0,0 +1,28 @@
+from webcam import webcam
+import json
+import numpy as np
+import cPickle
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
+def main():
+    data = {}
+
+    with open('data.json', 'r') as outfile:
+        data = cPickle.load(outfile)
+
+    wc = webcam(data)
+    wc.video_capture('Dulan')
+    # wc.video_capture('Dulan')
+    wc.video_capture('Sharada')
+
+    with open('data.json', 'w') as outfile:
+        cPickle.dump(wc.data, outfile)
+    # print type(wc.data)
+    # print wc.data
+
+if __name__=='__main__':
+    main()
---
+++ code/webcam.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+import sys
+import numpy as np
+
+class webcam():
+    def __init__(self, data={}):
+        self.vc =None
+        self.margin = 10
+        cascade_path = '../model/cv2/haarcascade_frontalface_alt2.xml'
+        self.cascade = cv.CascadeClassifier(cascade_path)
+        self.n_img_per_person = 2
+        self.data = data
+
+    def video_capture(self, name='Unknown'):
+        vc = cv.VideoCapture(0)
+        self.vc = cv.VideoCapture(0)
+        if vc.isOpened():
+            is_capturing, _ = vc.read()
+            print "Cam opened"
+        else:
+            is_capturing = False
+        key_input = ''
+        imgs = []
+        while is_capturing:
+            is_capturing, frame = vc.read()
+            frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            faces = self.cascade.detectMultiScale(frame,
+                                                  scaleFactor=1.1,
+                                                  minNeighbors=3,
+                                                  minSize=(100, 100))
+            if len(faces) != 0:
+                face = faces[0]
+                (x, y, w, h) = face
+                left = x - self.margin // 2
+                right = x + w + self.margin // 2
+                bottom = y - self.margin // 2
+                top = y + h + self.margin // 2
+                if key_input == ord('c'):
+                    img = cv.resize(frame[bottom:top, left:right, :],
+                                 (160, 160))
+                    imgs.append(img)
+                cv.rectangle(frame,
+                              (left - 1, bottom - 1),
+                              (right + 1, top + 1),
+                              (255, 0, 0), thickness=2)
+
+                txt_loc = (left, bottom + 20)
+                cv.putText(frame, name, txt_loc, cv.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 0), 2)
+
+            if len(imgs) == self.n_img_per_person:
+
+
+                capture_text = 'captured'
+                txt_loc = (left, bottom - 20)
+                cv.putText(frame, capture_text, txt_loc, cv.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 0), 2)
+                if name in self.data:
+                    capture_text = 'Duplicated'
+                    txt_loc = (left+120, bottom - 20)
+                    cv.putText(frame, capture_text, txt_loc, cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+                else:
+                    self.data[name] = imgs  # np.array(imgs)
+                cv.imshow('display', frame)
+                cv.waitKey(0)
+                vc.release()
+
+
+                break
+            frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+            cv.imshow('display', frame)
+            key_input = cv.waitKey(1)
+            if key_input == ord('q'):
+                sys.exit(0)
